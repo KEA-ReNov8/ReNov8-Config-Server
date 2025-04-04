30153879bee672d27fce0e736a9f6c2dbbf7101f
gohyeoncheol <huncher00@naver.com>
2025-04-04 09:12:02 +0900

    ✨ Feat : Config Server 완료

---
+++ src/test/kotlin/renov8/renov8configserver/ReNov8ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package renov8.renov8configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReNov8ConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
